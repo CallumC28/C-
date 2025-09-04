ba6a119f29d214235b15a47f8ccc3dd71a34d65a
CallumC28 <callumcummins28@gmail.com>
2025-09-04 02:13:59 +0100

    Messing about with C# syntax

--- Hello_World/Hello.cs
+++ Hello_World/Hello.cs
@@ -1,11 +1,16 @@
 using System;
 
-class HelloWorld
+namespace Hello_World //namespace is a way to organise code
 {
-    static void Main()
+    class HelloWorld
     {
-        Console.WriteLine("Hello, World!"); //writes the text in the console/terminal
+        static void Main()
+        {
+            Console.WriteLine("Hello"); //writes the text in the console/terminal
+            Console.Write("World"); //writes the text in the console/terminal without a new line
+            Console.Write("!"); //writes without a new line
+            Console.WriteLine("My name is Callum\nCummins"); //writes with a new line and \n creates a new line
+        }
     }
 }
-
 //to run the program do: dotnet run
